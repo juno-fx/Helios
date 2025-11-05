17ce0dade3fa534fbe503a023991339586829b84
Daniel Podwysocki <48068081+danielpodwysocki@users.noreply.github.com>
2025-11-05 17:40:13 +0100

    comment out the registration

--- common/root/etc/s6-overlay/s6-rc.d/init-hp-anywhere/start.sh
+++ common/root/etc/s6-overlay/s6-rc.d/init-hp-anywhere/start.sh
@@ -8,4 +8,4 @@ printf "${DISPLAY_DPI:-96}" >/run/s6/container_environment/DISPLAY_DPI
 printf "${DISPLAY_CDEPTH:-24}" >/run/s6/container_environment/DISPLAY_CDEPTH
 printf "/tmp/.XDG" >/run/s6/container_environment/XDG_RUNTIME_DIR
 
-pcoip-register-host --registration-code=${HP_ANYWHERE_REGISTRATION_CODE}
+# pcoip-register-host --registration-code=${HP_ANYWHERE_REGISTRATION_CODE}
